4c772802d565d715a4722f623dce6ab65bc88cd1
Jasper <japsai@gmail.com>
2019-03-05 09:46:59 +0100

    console module docstring added

--- glmdenoise/console.py
+++ glmdenoise/console.py
@@ -2,6 +2,12 @@ import argparse
 
 
 def main():
+    """This function gets called when the user executes `glmdenoise`.
+
+    It defines and interprets the console arguments, then calls
+    the relevant python code.
+    """
+
     parser = argparse.ArgumentParser(prog='glmdenoise')
     parser.add_argument('directory', nargs='?', default='.', 
         help='Data directory containing BIDS.')
