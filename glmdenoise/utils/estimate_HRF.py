77bdfe895f9a2b4276e571ee7e6434bcc77757ed
lajnd <p.j.d.lindh@uva.nl>
2019-03-05 11:18:10 +0100

    added notebook

---
+++ glmdenoise/utils/estimate_HRF.py
@@ -0,0 +1,24 @@
+"""
+f = normalisemax(m,dim)
+
+ <m> is a matrix
+ <dim> (optional) is the dimension of <m> to operate upon.
+   default to 2 if <m> is a row vector and to 1 otherwise.
+   special case is 0 which means operate globally.
+
+ divide <m> by the max value along some dimension (or globally).
+
+ example:
+ (normalisemax([[1, 2, 3]])==[[1/3, 2/3, 1]]).all()
+"""
+import numpy as np
+def normalisemax(m,dim=None):
+    # input
+    if dim is None:
+        dim = choose(isrowvector(m),1,0)
+    # do it
+    if dim==0:
+        f = m / np.max(m)
+    else:
+        f = m / np.max(m,dim)        
+    return f
