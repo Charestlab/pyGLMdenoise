6d26b00ed8df6b4f6490316baea32486f2299d17
Jasper <japsai@gmail.com>
2019-03-06 12:24:37 +0100

    docstring for BidsDirectory

--- glmdenoise/io/bids.py
+++ glmdenoise/io/bids.py
@@ -2,6 +2,9 @@ from bids import BIDSLayout
 
 
 class BidsDirectory(object):
+    """BIDS directory querying, currently a wrapper for pybids.BIDSLayout
+    """
+
 
     def __init__(self, directory):
         self.layout = BIDSLayout(directory, derivatives=True)
@@ -10,16 +13,34 @@ class BidsDirectory(object):
         return self.layout.get(return_type='id', target='subject')
     
     def get_tasks_for_subject(self, subject):
-        return self.layout.get(subject=subject, return_type='id', target='task')
+        return self.layout.get(
+            subject=subject,
+            return_type='id',
+            target='task'
+        )
 
     def get_filepaths_bold_runs(self, subject, task):
-        return self.layout.get(subject=subject, task=task, suffix='preproc', return_type='file')
+        return self.layout.get(
+            subject=subject,
+            task=task,
+            suffix='preproc',
+            return_type='file'
+        )
 
     def get_filepaths_event_runs(self, subject, task):
-        return self.layout.get(subject=subject, task=task, suffix='events', return_type='file')
+        return self.layout.get(
+            subject=subject,
+            task=task,
+            suffix='events',
+            return_type='file'
+        )
 
     def get_metas_bold_runs(self, subject, task):
-        runs = self.layout.get(subject=subject, task=task, suffix='bold') # get metadata from raw!?
+        runs = self.layout.get(
+            subject=subject,
+            task=task,
+            suffix='bold'  # get metadata from raw!?
+        )
         return [r.metadata for r in runs]
 
     def subject_id_from_number(self, sub_num):
