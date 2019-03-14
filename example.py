196eb560ea734beecb6e5ea65fda98f172fb06db
lajnd <p.j.d.lindh@uva.nl>
2019-03-14 17:49:58 +0100

    made GLMdenoise into as class. Included example.py

---
+++ example.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from glmdenoise.utils.make_design_matrix import make_design
+from glmdenoise import pyGlmdenoise as PYG
+import warnings
+import os, glob
+import nibabel as nib
+from itertools import compress
+warnings.simplefilter(action="ignore", category=FutureWarning)
+
+"""
+Load data
+"""
+
+fmri_folder = '/home/dlindh/AMS_AB_RSA/preproc/fmriprep/sub-01'
+
+
+runs = glob.glob(os.path.join(fmri_folder, 'ses*', 'func', '*preproc*nii.gz'))
+runs.sort()
+events = glob.glob(os.path.join(fmri_folder, 'ses*', 'func', '*_events.tsv'))
+events.sort()
+
+runs = compress(runs, np.arange(len(runs)) != 1)
+events = compress(events, np.arange(len(events)) != 1)
+
+data = []
+design = []
+hrf = np.load('hrf.npy')    
+for i, (run, event) in enumerate(zip(runs, events)):
+    print(f'run {i}')
+    y = nib.load(run).get_data()
+    y = np.moveaxis(y, -1,0)
+    y = y.reshape([y.shape[0], -1])
+
+    stimdur = 0.5
+    TR = 0.764
+    n_volumes = y.shape[0]
+
+    # Load onsets and item presented
+    onsets = pd.read_csv(event, sep='\t')["onset"].values
+    items = pd.read_csv(event, sep='\t')["item"].values
+    n_events = len(onsets)
+
+    # Create design matrix
+    events = pd.DataFrame()
+    events["duration"] = [stimdur] * n_events
+    events["onset"] = onsets
+    events["trial_type"] = items
+
+    X = make_design(events, TR, n_volumes, hrf)
+    data.append(y)
+    design.append(X)
+
+import time 
+GD = PYG.GLMdenoise(design, data, stim_dur=0.5, tr=0.764, n_jobs=1)
+start = time.time()
+GD.run()
+print(f'Fit took {time.time()-start} seconds!')
--- glmdenoise/pyGlmdenoise.py
+++ glmdenoise/pyGlmdenoise.py
@@ -2,12 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from make_design_matrix import make_design
+from glmdenoise.utils.make_design_matrix import make_design
 from itertools import compress
 from scipy.io import loadmat
 from tqdm import tqdm
 import warnings
-from utils.get_poly_matrix import *
+from joblib import Parallel, delayed
+from glmdenoise.utils.get_poly_matrix import *
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
 def R2_nom_denom(y, yhat):
@@ -26,9 +27,37 @@ def R2_nom_denom(y, yhat):
         denom = np.sum(y ** 2, axis=0)  # Kendricks denominator
     return nom, denom
 
+def whiten_data(data, design, extra_regressors=False, poly_degs=np.arange(5)):
+    """[summary]
+    
+    Arguments:
+        data {[type]} -- [description]
+        design {[type]} -- [description]
+    
+    Keyword Arguments:
+        extra_regressors {bool} -- [description] (default: {False})
+        poly_degs {[type]} -- [description] (default: {np.arange(5)})
+    
+    Returns:
+        [type] -- [description]
+    """
 
-def fit_runs(runs, DM, extra_regressors=False, poly_degs=np.arange(5)):
-    """Fits a least square of combined runs by first whitening the data and design. 
+
+    # whiten data
+    whitened_data = []
+    whitened_design = []
+    for i, (y, X) in enumerate(zip(data, design)):
+        polynomials = get_poly_matrix(X.shape[0], poly_degs)
+        if extra_regressors and extra_regressors[0].any():
+            polynomials = np.c_[polynomials, extra_regressors[i]]
+        
+        whitened_design.append(make_project_matrix(polynomials) @ X)
+        whitened_data.append(make_project_matrix(polynomials) @ y)
+
+    return whitened_data, whitened_design
+    
+def fit_runs(data, design):
+    """Fits a least square of combined runs. 
        The matrix addition is equivalent to concatenating the list of data and the list of
        design and fit it all at once. However, this is more memory efficient. 
     Arguments:
@@ -36,175 +65,181 @@ def fit_runs(runs, DM, extra_regressors=False, poly_degs=np.arange(5)):
         DM {list} -- List of design matrices. Each design matrix 
                      is an TR x predictor sizec array
     
-    Keyword Arguments:
-        extra_regressors {list} -- list of same length as runs and DM (default: {False})
-        poly_degs {array} -- array of polynomial degrees to project 
-                             from data and design (default: {np.arange(5)})
-    
     Returns:
         [array] -- betas from fit
     """
-
-    # whiten data
-    for i, (y, X) in enumerate(zip(runs, DM)):
-        polynomials = get_poly_matrix(X.shape[0], poly_degs)
-        if extra_regressors and extra_regressors[0].any():
-            polynomials = np.c_[polynomials, extra_regressors[i]]
-        DM[i] = make_project_matrix(polynomials) @ X
-        runs[i] = make_project_matrix(polynomials) @ y
     
-    X = np.vstack(DM)
+    X = np.vstack(design)
     X = np.linalg.inv(X.T @ X) @ X.T
 
     betas = 0
     start_col = 0
-    for run in (runs):
+
+    for run in (data):
         n_vols = run.shape[0]
         these_cols = np.arange(n_vols) + start_col
         betas += X[:, these_cols] @ run
         start_col += run.shape[0]
+
     return betas
 
-def cross_validate(data, design, extra_regressors=False, poly_degs=np.arange(5)):
+
+
+class GLMdenoise():
     """[summary]
-    
-    Arguments:
-        data {list} -- [description]
-        design {list} -- [description]
-    
-    Keyword Arguments:
-        extra_regressors {bool/list} -- [description] (default: {False})
-    
-    Returns:
-        [array] -- [description]
     """
 
-    nom_denom = []
-    for run in tqdm(range(n_runs), desc='Cross-validating run'):
-        # fit data using all the other runs
-        mask = np.arange(n_runs) != run
-        if extra_regressors and extra_regressors[0].any():
+    def __init__(self, design, data, stim_dur=0.5, tr=2, n_jobs=10, n_pcs=20, n_boots=100):
+        
+        self.design = design
+        self.data = data
+        self.stim_dur = stim_dur
+        self.tr = tr
+        self.n_pcs = n_pcs
+        self.dims = data[0].shape
+        self.n_jobs = n_jobs
+        self.n_boots = n_boots
+        self.n_runs = len(data)
+        # calculate polynomial degrees
+        max_poly_deg = int(((data[0].shape[0] * tr) / 60) // 2) + 1
+        self.poly_degs = np.arange(max_poly_deg)
+
+
+        
+        self.mean_image = np.vstack(data).mean(0).reshape(*self.dims[1:])
+        self.mean_mask = self.mean_image > np.percentile(self.mean_image, 99) / 2
+        self.flat_mean_mask = self.mean_mask.flatten()
+        # reduce data
+        self.data = [d[:, self.flat_mean_mask].astype(np.float16) for d in self.data]
+
+    def cross_validate(self, extra_regressors=False):
+        """[summary]
+        
+        Arguments:
+            data {list} -- [description]
+            design {list} -- [description]
+        
+        Keyword Arguments:
+            extra_regressors {bool/list} -- [description] (default: {False})
+        
+        Returns:
+            [array] -- [description]
+        """
+
+        whitened_data, whitened_design = whiten_data(self.data, self.design, extra_regressors, self.poly_degs)
+
+        n_runs = len(self.data)
+        nom_denom = []
+        for run in tqdm(range(n_runs), desc='Cross-validating run'):
+            # fit data using all the other runs
+            mask = np.arange(n_runs) != run
+
             betas = fit_runs(
-                list(compress(data, mask)),
-                list(compress(design, mask)),
-                list(compress(extra_regressors, mask)))
-        else:
-            betas = fit_runs(
-                list(compress(data, mask)),
-                list(compress(design, mask)))
+                list(compress(whitened_data, mask)),
+                list(compress(whitened_design, mask)))
 
-        # predict left-out run with vanilla design matrix
-        yhat = design[run] @ betas
+            # predict left-out run with vanilla design matrix
+            yhat = self.design[run] @ betas
 
-        y = data[run]
-        # get polynomials
-        polynomials = get_poly_matrix(y.shape[0], poly_degs)
-        # project out polynomials from data and prediction
-        y = make_project_matrix(polynomials) @ y 
-        yhat = make_project_matrix(polynomials) @ yhat
+            y = self.data[run]
+            # get polynomials
+            polynomials = get_poly_matrix(y.shape[0], self.poly_degs)
+            # project out polynomials from data and prediction
+            y = make_project_matrix(polynomials) @ y 
+            yhat = make_project_matrix(polynomials) @ yhat
 
-        nom, denom = R2_nom_denom(y, yhat)
-        nom_denom.append((nom, denom))
-    
-    # calculate R2
-    nom = np.array(nom_denom).sum(0)[0, :]
-    denom = np.array(nom_denom).sum(0)[1, :]
-    with np.errstate(divide="ignore", invalid="ignore"):
-        r2s = np.nan_to_num(1 - (nom / denom)) * 100
-    return r2s
+            nom, denom = R2_nom_denom(y, yhat)
+            nom_denom.append((nom, denom))
+        
+        # calculate R2
+        nom = np.array(nom_denom).sum(0)[0, :]
+        denom = np.array(nom_denom).sum(0)[1, :]
+        with np.errstate(divide="ignore", invalid="ignore"):
+            r2s = np.nan_to_num(1 - (nom / denom)) * 100
+        return r2s
 
-"""
-Load data
-"""
-data = []
-design = []
-n_runs = 6
-hrf = np.load('hrf.npy')    
-for ii in range(n_runs):
-    y = np.load(f"data/sub_sub-01_slice_15_run_{ii+1:02d}.npy")
-    #y = np.moveaxis(y, 2,0)
-    y = np.swapaxes(y, 0, 2)
-    dims = y.shape
-    y = y.reshape([y.shape[0], -1])
+    def run(self):
+        """
+        """
+        print('Making initial fit')
+        r2s_initial= self.cross_validate()
+        print('Done!')
 
-    stimdur = 0.5
-    TR = 0.764
-    n_scans = y.shape[0]
-    frame_times = np.arange(n_scans) * TR
+        print('Getting noise pool')
+        noise_pool_mask = (r2s_initial < 0)
 
-    # Load onsets and item presented
-    onsets = pd.read_csv(f"data/sub_sub-01_slice_15_run_{ii+1:02d}.csv")[
-        "onset"
-    ].values
-    items = pd.read_csv(f"data/sub_sub-01_slice_15_run_{ii+1:02d}.csv")[
-        "item"
-    ].values
-    n_events = len(onsets)
+        print('Calculating PCs...')
+        run_PCAs = []
+        for run in self.data:
+            noise_pool = run[:, noise_pool_mask]
 
-    # Create design matrix
-    events = pd.DataFrame()
-    events["duration"] = [stimdur] * n_events
-    events["onset"] = onsets
-    events["trial_type"] = items
+            polynomials = get_poly_matrix(noise_pool.shape[0], self.poly_degs)
+            noise_pool = make_project_matrix(polynomials) @ noise_pool
 
-    X = make_design(events, TR, n_scans, hrf)
-    data.append(y)
-    design.append(X)
+            noise_pool = normalize(noise_pool, axis=0)
 
-max_poly_deg = int(((data[0].shape[0] * TR) / 60) // 2) + 1
-poly_degs = np.arange(max_poly_deg)
+            noise_pool = noise_pool @ noise_pool.T
+            u, s, vt = np.linalg.svd(noise_pool)
+            u =  u[:, :21]
+            u = u / np.std(u, 0)
+            run_PCAs.append(u)
 
-"""
-mean data and mask
-"""
-mean_image = np.vstack(data).mean(0).reshape(*dims[1:])
-mean_mask = mean_image > np.percentile(mean_image, 99) / 2
+        self.pc_regressors = []
+        for n_pc in range(self.n_pcs):
+            self.pc_regressors.append([pc[:, :n_pc] for pc in run_PCAs])
+        print('Done!')
 
-"""
-Get initial fit to select noise pool
-"""
-r2s_vanilla = cross_validate(data, design)
 
-"""
-get noise pool
-"""
-mask_flat = mean_mask.reshape(-1)
-noise_pool_mask = (r2s_vanilla < 0) & mask_flat
+        print('Fit data with PCs...')
+        all_r2s =  Parallel(n_jobs=self.n_jobs)(
+                    delayed(self.cross_validate)(
+                        self.pc_regressors[x]) for x in tqdm(
+                            range(self.n_pcs), desc='Number of PCs'))
+        print('Done!')
 
-"""
-Get PCAs
-"""
-run_PCAs = []
-for run in data:
-    noise_pool = run[:, noise_pool_mask]
+        # calculate best number of PCs
+        all_r2s = np.asarray(all_r2s)
+        best_mask = np.any(all_r2s > 0, 0)
+        xval  = np.nanmedian(all_r2s[:, best_mask], 1)
+        select_pca = select_noise_regressors(np.asarray(xval))
+        print(f'Selected {select_pca} number of PCs')
 
-    polynomials = get_poly_matrix(noise_pool.shape[0], poly_degs)
-    noise_pool = make_project_matrix(polynomials) * np.mat(noise_pool)
+        # plt.plot(xval)
+        # plt.plot(select_pca, xval[select_pca], "o")
+        # plt.show()
 
-    noise_pool = normalize(noise_pool, axis=0)
+        whitened_data, whitened_design = whiten_data(
+                        self.data, self.design, self.pc_regressors[select_pca], self.poly_degs)
 
-    noise_pool = noise_pool @ noise_pool.T
-    u, s, vt = np.linalg.svd(noise_pool)
-    u =  u[:, :21]
-    u = u / np.std(u, 0)
-    run_PCAs.append(u)
+        print('Bootstrapping betas...')
+        n_runs = len(self.data)
+        boot_betas = []
 
-"""
-Get R2 per PCn
-"""
-all_r2s = []
-for n_pca in tqdm(range(21), desc='Number of PCs'):
-    pc_regressors = [pc[:, :n_pca] for pc in run_PCAs]
-    all_r2s.append(cross_validate(data, design, pc_regressors))
-all_r2s = np.array(all_r2s)
+        boot_data = []
+        boot_design = []
+        for b in range(self.n_boots):
+            boot_inds = np.random.choice(np.arange(n_runs), n_runs)
+            boot_data.append([whitened_data[ind] for ind in boot_inds])
+            boot_design.append([whitened_design[ind] for ind in boot_inds])
 
-"""
-Plot R2 per PCn 
-"""
-best_mask = np.any(all_r2s > 0, 0) & mask_flat
-xval  = np.nanmedian(all_r2s[:, best_mask], 1)
-select_pca = select_noise_regressors(np.asarray(xval))
+        boot_betas =  Parallel(n_jobs=self.n_jobs)(
+                    delayed(fit_runs)(
+                        boot_data[x], boot_design[x]) for x in tqdm(
+                            range(self.n_boots), desc='Bootstrapping'))
+        print('Done!')
 
-plt.plot(xval)
-plt.plot(select_pca, xval[select_pca], "o")
-plt.show()
+
+        boot_betas = np.array(boot_betas)
+        final_fit = np.median(boot_betas, 0)
+        n_conds = boot_betas.shape[1]
+        standard_error = np.zeros((final_fit.shape))
+        for cond in range(n_conds):
+            percentiles = np.percentile(boot_betas[:, cond, :], [16, 84], axis=0)
+            standard_error[cond, :] = (percentiles[1, :] - percentiles[0, :])/2
+
+        #poolse = np.sqrt(np.mean(standard_error**2, axis=0))
+        with np.errstate(divide="ignore", invalid="ignore"):
+            self.pseudo_t_stats = final_fit / standard_error
+
+        # sns.heatmap(pseudo_t_stats.mean(0).reshape((80,80)), xticklabels=False, yticklabels=False)
+        # plt.show()
