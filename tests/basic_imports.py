216427b7c9a183eb23eb63de33f5a59c601ca697
Ian Charest (School of Psychology) <i.charest@bham.ac.uk>
2019-03-18 09:20:30 +0000

    making travis happy

--- tests/basic_imports.py
+++ tests/basic_imports.py
@@ -6,9 +6,4 @@ class BasicImportsTest(TestCase):
 
     @skip('main function not ready yet')
     def test_main_function_runs(self):
-<<<<<<< HEAD
         pass
-=======
-        from glmdenoise.data import run_data
-        out = run_data(numpy.array([]), numpy.array([]), 0, 0)
->>>>>>> master
