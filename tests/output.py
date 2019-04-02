e32f2ca441a698b7d96f202bfc08cb9cbd549ba4
Jasper <vandejjf@bham.ac.uk>
2019-04-02 19:06:04 +0100

    making intermediate directories

--- tests/output.py
+++ tests/output.py
@@ -6,14 +6,14 @@ class OutputTests(TestCase):
 
     @patch('glmdenoise.io.output.nibabel')
     @patch('glmdenoise.io.output.numpy')
-    @patch('glmdenoise.io.output.mkdir')
-    def test_ensures_directory(self, mkdir, _np, _nb):
+    @patch('glmdenoise.io.output.makedirs')
+    def test_ensures_directory(self, makedirs, _np, _nb):
         from glmdenoise.io.output import Output
         output = Output()
         filepath='/home/johndoe/data/myproject/run_1.nii'
         output.configure_from(sample_file=filepath)
         output.save_variable(11, 'foo')
-        mkdir.assert_called_with(
+        makedirs.assert_called_with(
             '/home/johndoe/data/myproject/glmdenoise'
         )
 
