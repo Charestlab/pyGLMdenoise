acfd8521a4d41278c26cba64b6c4bfb9ac8ededd
Jasper <japsai@gmail.com>
2019-04-07 22:26:43 +0100

    new version: 0.0.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, pyGLMdenoise authors'
 author = 'pyGLMdenoise authors'
 
 # The short X.Y version
-version = ''
+version = '0.0.3'
 # The full version, including alpha/beta/rc tags
-release = '0.0'
+release = version
 
 
 # -- General configuration ---------------------------------------------------
